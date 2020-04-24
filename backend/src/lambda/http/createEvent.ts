f0eb0998967dec2e870fdceff7f461211c4faa22
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-24 08:29:45 +0200

    feat(app): set serverless.yml and dummy http method

---
+++ backend/src/lambda/http/createEvent.ts
@@ -0,0 +1,16 @@
+  
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  console.log('create new Event')
+  
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({item: 'first response!'})
+  }
+}
