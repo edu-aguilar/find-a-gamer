1b9c48eb3b858074ddc8b0ca04a269bfb02eb665
Edu Aguilar <eduardo.aguilar@nc43tech.com>
2020-04-29 08:40:01 +0200

    feat(gameSelector): create gameSelector component

---
+++ frontend/src/components/gameSelector.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="game-selector">
+    <select
+      name="game-selector"
+      id="game-selector"
+      @change="gameChanged($event)"
+    >
+      <option :value="index" v-for="(item, index) in items" :key="index">
+        <span>{{ item.acronym }}</span>
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "game-selector",
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    gameChanged(event) {
+      const optionValue = event.target.value;
+      this.$emit("game-changed", this.items[optionValue]);
+    }
+  }
+};
+</script>
+
+<style></style>
